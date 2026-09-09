c37e20a3cc2c3029715487b9f5eda8666f8b3a28
z76694997-lgtm <z76694997@gmail.com>
2026-09-09 17:16:16 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# Основные токены и ID доступа
+BOT_TOKEN = "8876639758:AAFXOoDLyFd2C9B90QQlGRsd4-RnUMvqiZo"
+ADMIN_ID = 8669477816
+
+# Данные приложения Telegram (API)
+API_ID = 39188918
+API_HASH = "41aaeaa0c6f9a61c0504395ccf5f3b3c"
