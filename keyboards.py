af3697976d2e586ee1e270a3275412c3d5452b57
z76694997-lgtm <z76694997@gmail.com>
2026-09-09 17:20:23 +0300

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,14 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+def admin_kb():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="📊 Статистика базы", callback_data="stats")],
+        [InlineKeyboardButton(text="📱 Список всех номеров", callback_data="admin_view_all")],
+        [InlineKeyboardButton(text="📥 Инструкция", callback_data="help")]
+    ])
+
+def user_kb():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="🎁 Мои аккаунты", callback_data="user_my_accs")]
+    ])
+  
